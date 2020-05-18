2198d4010b2a1beaefb057d24cf01beafd643b46
Christopher Osborn <z68ozz@gmail.com>
2020-05-18 03:46:50 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre 
+  
+  attr_accessor :name, :artist, :song
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  
+  def songs 
+    Song.all.select {|s| s.genre == self}
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+  def artists
+    songs.map {|s| s.artist}
+  end 
+
+  
+end 
